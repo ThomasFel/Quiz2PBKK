abefe0e7da96419f79e8af4a3af9ec05d9f326c8
daffainfo <md15ev@gmail.com>
2022-05-03 12:33:34 +0700

    First commit, add adminlte and fix routes

---
+++ src/main/java/com/blockdev/pbkk/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.blockdev.pbkk.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String index() {
+        return "static/index";
+    }
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "static/profile";
+    }
+
+    @GetMapping("/company")
+    public String company() {
+        return "static/company";
+    }
+
+    @GetMapping("/service")
+    public String service() {
+        return "static/service";
+    }
+
+    @GetMapping("/collaboration")
+    public String collaboration() {
+        return "static/collaboration";
+    }
+
+}
