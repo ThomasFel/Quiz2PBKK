eccbbcff49efe0e42b2bc90d181882f4d642516f
Thomas Felix <thomasfelixbrilliant@gmail.com>
2022-05-11 00:12:58 +0700

    fix: layout design

--- src/main/java/com/blockdev/pbkk/security/WebSecurityConfig.java
+++ src/main/java/com/blockdev/pbkk/security/WebSecurityConfig.java
@@ -2,21 +2,12 @@ package com.blockdev.pbkk.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import javax.sql.DataSource;
-import java.util.ArrayList;
-import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -28,7 +19,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/bower_components/**", "/dist/**", "/plugins/**", "/", "/profile", "/company", "/service", "/collaboration", "/register").permitAll()
+                .antMatchers("/bower_components/**", "/dist/**", "/plugins/**", "/vendor/**", "/image/**", "/", "/profile", "/company", "/service", "/collaboration", "/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -42,5 +33,4 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .logoutSuccessUrl("/login")
                 .permitAll();
     }
-
 }
